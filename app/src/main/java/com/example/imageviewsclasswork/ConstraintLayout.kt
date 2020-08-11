a1e9141f3dd0bbc97a606eefa3e4d2b0b564554f
Ransom Ahanmisi <60149483+Ransom8@users.noreply.github.com>
2020-08-11 09:00:41 +0100

    completed task

---
+++ app/src/main/java/com/example/imageviewsclasswork/ConstraintLayout.kt
@@ -0,0 +1,13 @@
+package com.example.imageviewsclasswork
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ConstraintLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.constraint_layout)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/imageviewsclasswork/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.imageviewsclasswork
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.linear_layout.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_layout)
+
+//        button7.setOnClickListener {
+//            Intent(this, ConstraintLayout::class.java).also {
+//                startActivity(it)
+//            }
+//        }
+    }
+}
